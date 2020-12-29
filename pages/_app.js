f0c174de78b305e6537538cbf14c73325a80228a
David <sanderdj90@gmail.com>
2020-12-28 21:06:06 -0700

    feat: nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/index.css'
 import { Auth0Provider } from '@auth0/auth0-react';
+import Nav from "../components/nav"
 function MyApp({ Component, pageProps }) {
   return (
     <Auth0Provider
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
       clientId={process.env.AUTH0_CLIENTID}
       redirectUri={process.env.REDIRECT_URI}
     >
+      <Nav/>
       <Component {...pageProps} />
     </Auth0Provider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
 export default function Home() {
-  const {
-    isLoading,
-    isAuthenticated,
-    error,
-    user,
-    loginWithRedirect,
-    logout,
-  } = useAuth0();
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
-  if (isAuthenticated) {
-    console.log(user);
-    return (
-      <div className="flex flex-row justify-end items-center space-x-4 bg-teal-500 text-white mx-8">
-        <span>
-          Hello, <strong>{user.name}</strong>{" "}
-        </span>
-        <img className="my-4 h-10 rounded" src={user.picture} alt="" />
-        <button
-          className="btn-blue my-4"
-          onClick={() => logout({ returnTo: window.location.origin })}
-        >
-          Log out
-        </button>
-      </div>
-    );
-  } else {
-    return <button onClick={loginWithRedirect}>Log in</button>;
-  }
+  return (
+    <div className="text-white">
+      {" "}
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam debitis,
+      tenetur cupiditate sequi, voluptas, accusantium error officiis
+      reprehenderit eius eaque qui nostrum earum minus sunt. Culpa quam error
+      quas sit.
+    </div>
+  );
 }
